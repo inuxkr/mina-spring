61b7756a70c912972fed16495df801e8b11b4b2e
Java2Enterprise <Java2Enterprise@917403fc-bc81-11dd-b7f4-ab9653f8cbb3>
2008-11-27 15:49:41 +0000

    Add apache license declaration for all files

--- core/src/org/springframework/remoting/mina/MinaServiceClientHandler.java
+++ core/src/org/springframework/remoting/mina/MinaServiceClientHandler.java
@@ -1,7 +1,26 @@
+/*
+ * Copyright 2008-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 package org.springframework.remoting.mina;
 
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IoSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xl.util.concurrent.BlockingMap;
 
 /**
@@ -12,6 +31,8 @@ import org.xl.util.concurrent.BlockingMap;
  */
 public class MinaServiceClientHandler extends IoHandlerAdapter {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+	
 	private BlockingMap<ReturnAddress, ReturnAddressAwareRemoteInvocationResult> results 
 		= new BlockingMap<ReturnAddress, ReturnAddressAwareRemoteInvocationResult>();
 	
@@ -19,14 +40,18 @@ public class MinaServiceClientHandler extends IoHandlerAdapter {
 	public void messageReceived(IoSession session, Object message)
 		throws Exception {
 		
-		System.out.println("Received message " + message);
+		if (logger.isDebugEnabled()) {
+			logger.trace("Message received : " + message);
+		}
 		ReturnAddressAwareRemoteInvocationResult result = (ReturnAddressAwareRemoteInvocationResult) message;
 		results.put(result.getReturnAddress(), result);
 	}
 
 	@Override
 	public void messageSent(IoSession session, Object message) throws Exception {
-		System.out.println("Sent message " + message);
+		if (logger.isDebugEnabled()) {
+			logger.trace("Message sent : " + message);
+		}		
 	}
 	
 	public ReturnAddressAwareRemoteInvocationResult getReceivedMessage(ReturnAddress returnAddress) {
@@ -34,7 +59,11 @@ public class MinaServiceClientHandler extends IoHandlerAdapter {
 			ReturnAddressAwareRemoteInvocationResult invocation = (ReturnAddressAwareRemoteInvocationResult) results.take(returnAddress);
 			return invocation;
 		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
+			String message = "Receive Message failed : " + e.getMessage();
+			if (logger.isErrorEnabled()) {
+				logger.error(message, e);
+			}
+			throw new RuntimeException(message, e);
 		}
 	}
 
